c274c97b6abac6d59f06b932bdeeae644606587f
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-17 13:11:35 -0500

    test: :construction: esqueleto inicial de las pruebas unitarias

--- tests/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HelloWorld } from '../src';
-
-describe('App', () => {
-  it('should create the app', () => {
-    const app = new HelloWorld();
-    app.sayHello('test domain');
-    expect(app).toBeTruthy();
-  });
-});
