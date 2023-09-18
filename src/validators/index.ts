15b9d2e98eeee121cad0af3e0d2a7cb9b11159bd
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-18 10:11:28 -0500

    fix: :label: se actualiza un archivo de barril en los validadores

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,1 +1,5 @@
 export * from './base';
+export * from './complete-to-do.validator';
+export * from './create-to-do.validator';
+export * from './login.validator';
+export * from './new-user.validator';
