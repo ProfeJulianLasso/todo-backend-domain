893148bc7581289561482220b38898521fb6553e
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-18 17:52:26 -0500

    test: :white_check_mark: se agregan pruenas unitarias para las entidades

--- tests/entities/to-do.entity.spec.ts
+++ tests/entities/to-do.entity.spec.ts
@@ -1,5 +1,206 @@
+import {
+  CompletedValueObject,
+  StatusValueObject,
+  ToDo,
+  ToDoDescriptionValueObject,
+  ToDoIdValueObject,
+  ToDoTitleValueObject,
+  ToDoType,
+  User,
+  UserEmailValueObject,
+  UserIdValueObject,
+  UserNameValueObject,
+  UserPasswordValueObject,
+} from '../../src';
+
 describe('ToDoEntity', () => {
-  it('should to be valid', () => {
-    expect(true).toBe(true);
+  let toDoId: ToDoIdValueObject;
+  let user: User;
+  let title: ToDoTitleValueObject;
+  let description: ToDoDescriptionValueObject;
+  let completed: CompletedValueObject;
+  let status: StatusValueObject;
+
+  beforeEach(() => {
+    toDoId = new ToDoIdValueObject('550e8400-e29b-41d4-a716-887744331234');
+    user = new User({
+      userId: new UserIdValueObject('550e8400-e29b-41d4-a716-887744331234'),
+      name: new UserNameValueObject('test name'),
+      email: new UserEmailValueObject('test@domain.co'),
+      password: new UserPasswordValueObject('123456'),
+    });
+    title = new ToDoTitleValueObject('test title');
+    description = new ToDoDescriptionValueObject('test description');
+    completed = new CompletedValueObject(false);
+    status = new StatusValueObject(true);
+  });
+
+  it('should create a valid ToDoEntity', () => {
+    // Arrange
+    const data = {
+      toDoId,
+      user,
+      title,
+      description,
+      completed,
+      status,
+    } as ToDoType;
+
+    // Act
+    const toDo = new ToDo(data);
+
+    // Assert
+    expect(toDo).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a valid ToDoEntity with a valid toDoId', () => {
+      // Arrange
+      const expected = '550e8400-e29b-41d4-a716-887744331234';
+      const data = {
+        toDoId,
+        title,
+        description,
+        completed,
+        status,
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.create(user);
+
+      // Assert
+      expect(toDo.toDoId.value).toEqual(expected);
+      expect(toDo.user.userId.value).toEqual(expected);
+    });
+
+    it('should create a valid ToDoEntity without a toDoId', () => {
+      // Arrange
+      const regex = /^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/i;
+      const data = {
+        title,
+        description,
+        completed,
+        status,
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.create(user);
+
+      // Assert
+      expect(toDo.toDoId.value).toMatch(regex);
+      expect(toDo.user.userId.value).toMatch(regex);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a valid ToDoEntity', () => {
+      // Arrange
+      const expectedTitle = 'updated title';
+      const expectedDescription = 'updated description';
+      const expectedCompleted = true;
+      const data = {
+        title,
+        description,
+        completed,
+        status,
+      } as ToDoType;
+      const updatedData = {
+        title: new ToDoTitleValueObject('updated title'),
+        description: new ToDoDescriptionValueObject('updated description'),
+        completed: new CompletedValueObject(true),
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.update(updatedData);
+
+      // Assert
+      expect(toDo.title.value).toEqual(expectedTitle);
+      if (toDo.description)
+        expect(toDo.description.value).toEqual(expectedDescription);
+      expect(toDo.completed.value).toEqual(expectedCompleted);
+    });
+  });
+
+  describe('complete', () => {
+    it('should complete a valid ToDoEntity', () => {
+      // Arrange
+      const expected = true;
+      const data = {
+        title,
+        description,
+        completed,
+        status,
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.complete();
+
+      // Assert
+      expect(toDo.completed.value).toEqual(expected);
+    });
+  });
+
+  describe('incomplete', () => {
+    it('should incomplete a valid ToDoEntity', () => {
+      // Arrange
+      const expected = false;
+      const data = {
+        title,
+        description,
+        completed: new CompletedValueObject(true),
+        status,
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.incomplete();
+
+      // Assert
+      expect(toDo.completed.value).toEqual(expected);
+    });
+  });
+
+  describe('enable', () => {
+    it('should enable a valid ToDoEntity', () => {
+      // Arrange
+      const expected = true;
+      const data = {
+        title,
+        description,
+        completed,
+        status: new StatusValueObject(false),
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.enable();
+
+      // Assert
+      expect(toDo.status.value).toEqual(expected);
+    });
+  });
+
+  describe('disable', () => {
+    it('should disable a valid ToDoEntity', () => {
+      // Arrange
+      const expected = false;
+      const data = {
+        title,
+        description,
+        completed,
+        status,
+      } as ToDoType;
+
+      // Act
+      const toDo = new ToDo(data);
+      toDo.disable();
+
+      // Assert
+      expect(toDo.status.value).toEqual(expected);
+    });
   });
 });
--- tests/entities/user.entity.spec.ts
+++ tests/entities/user.entity.spec.ts
@@ -1,5 +1,186 @@
+import {
+  StatusValueObject,
+  ToDo,
+  ToDoDescriptionValueObject,
+  ToDoIdValueObject,
+  ToDoTitleValueObject,
+  User,
+  UserEmailValueObject,
+  UserIdValueObject,
+  UserNameValueObject,
+  UserPasswordValueObject,
+  UserType,
+} from '../../src';
+
 describe('UserEntity', () => {
-  it('should to be valid', () => {
-    expect(true).toBe(true);
+  let userId: string;
+  let name: string;
+  let email: string;
+  let password: string;
+  let toDos: ToDo[];
+
+  beforeEach(() => {
+    userId = '550e8400-e29b-41d4-a716-887744331234';
+    name = 'test name';
+    email = 'test@domain.co';
+    password = '123456';
+    toDos = [];
+  });
+
+  it('should create a valid UserEntity', () => {
+    // Arrange
+    const data = {
+      userId: new UserIdValueObject(userId),
+      name: new UserNameValueObject(name),
+      email: new UserEmailValueObject(email),
+      password: new UserPasswordValueObject(password),
+      toDos,
+    } as UserType;
+
+    // Act
+    const user = new User(data);
+
+    // Assert
+    expect(user).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a valid UserEntity with a valid userId', () => {
+      // Arrange
+      const expected = true;
+      const expectedToDos = Array<ToDo>;
+      const data = {
+        userId: new UserIdValueObject(userId),
+        name: new UserNameValueObject(name),
+        email: new UserEmailValueObject(email),
+        password: new UserPasswordValueObject(password),
+      } as UserType;
+
+      // Act
+      const user = new User(data);
+      user.create();
+
+      // Assert
+      expect(user.status.value).toBe(expected);
+      expect(user.toDos).toBeInstanceOf(expectedToDos);
+    });
+
+    it('should create a valid UserEntity without a userId', () => {
+      // Arrange
+      const regex = /^[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}$/i;
+      const expected = true;
+      const expectedToDos = Array<ToDo>;
+      const data = {
+        name: new UserNameValueObject(name),
+        email: new UserEmailValueObject(email),
+        password: new UserPasswordValueObject(password),
+      } as UserType;
+
+      // Act
+      const user = new User(data);
+      user.create();
+
+      // Assert
+      expect(user.userId.value).toMatch(regex);
+      expect(user.status.value).toBe(expected);
+      expect(user.toDos).toBeInstanceOf(expectedToDos);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a valid UserEntity', () => {
+      // Arrange
+      const expectedName = 'updated name';
+      const expectedEmail = 'new_email_test@domain.co';
+      const expectedPass = 'newPass123!';
+      const newName = new UserNameValueObject('updated name');
+      const newEmail = new UserEmailValueObject('new_email_test@domain.co');
+      const newPass = new UserPasswordValueObject('newPass123!');
+      const currentData = {
+        userId: new UserIdValueObject(userId),
+        name: new UserNameValueObject('updated name'),
+        email: new UserEmailValueObject(email),
+        password: new UserPasswordValueObject(password),
+      } as UserType;
+      const newData = {
+        name: newName,
+        email: newEmail,
+        password: newPass,
+      } as UserType;
+
+      // Act
+      const user = new User(currentData);
+      user.update(newData);
+
+      // Assert
+      expect(user.name.value).toBe(expectedName);
+      expect(user.email.value).toBe(expectedEmail);
+      expect(user.password.value).toBe(expectedPass);
+    });
+  });
+
+  describe('enable', () => {
+    it('should enable a valid UserEntity', () => {
+      // Arrange
+      jest
+        .spyOn(ToDo.prototype, 'enable')
+        .mockImplementation(() => new ToDo({}));
+      const toDos = [
+        new ToDo({
+          toDoId: new ToDoIdValueObject('550e8400-e29b-41d4-a716-887744331234'),
+          title: new ToDoTitleValueObject('test name'),
+          description: new ToDoDescriptionValueObject('test description'),
+          status: new StatusValueObject(false),
+        }),
+      ] as ToDo[];
+      const expected = true;
+      const currentData = {
+        userId: new UserIdValueObject(userId),
+        name: new UserNameValueObject(name),
+        email: new UserEmailValueObject(email),
+        password: new UserPasswordValueObject(password),
+        toDos,
+      } as UserType;
+
+      // Act
+      const user = new User(currentData);
+      user.enable();
+
+      // Assert
+      expect(user.status.value).toBe(expected);
+      expect(ToDo.prototype.enable).toHaveBeenCalled();
+    });
+  });
+
+  describe('disable', () => {
+    it('should disable a valid UserEntity', () => {
+      // Arrange
+      jest
+        .spyOn(ToDo.prototype, 'disable')
+        .mockImplementation(() => new ToDo({}));
+      const toDos = [
+        new ToDo({
+          toDoId: new ToDoIdValueObject('550e8400-e29b-41d4-a716-887744331234'),
+          title: new ToDoTitleValueObject('test name'),
+          description: new ToDoDescriptionValueObject('test description'),
+        }),
+      ] as ToDo[];
+      const expected = false;
+      const currentData = {
+        userId: new UserIdValueObject(userId),
+        name: new UserNameValueObject(name),
+        email: new UserEmailValueObject(email),
+        password: new UserPasswordValueObject(password),
+        toDos,
+      } as UserType;
+
+      // Act
+      const user = new User(currentData);
+      user.disable();
+
+      // Assert
+      expect(user.status.value).toBe(expected);
+      expect(ToDo.prototype.disable).toHaveBeenCalled();
+    });
   });
 });
