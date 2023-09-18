2644cc21eaee276004fa6036895bcde4bb23ced5
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-18 10:47:46 -0500

    test: :white_check_mark: se agrega prueba unitaria de DomainException

--- tests/exceptions/domain.exception.spec.ts
+++ tests/exceptions/domain.exception.spec.ts
@@ -1,5 +1,40 @@
+import { DomainException, ValueObjectExceptionType } from '../../src';
+
 describe('DomainException', () => {
-  it('should to be valid', () => {
-    expect(true).toBe(true);
+  it('should get the message "invalid data"', () => {
+    // Arrange
+    const messageForException = 'Invalid data';
+    const expectedThrowMessage = 'Invalid data';
+    const exception = new DomainException(messageForException);
+
+    // Act
+    const message = exception.message;
+
+    // Assert
+    expect(message).toBe(expectedThrowMessage);
+  });
+
+  it('should get the details of the error', () => {
+    // Arrange
+    const messageForException = 'Invalid data';
+    const details = [
+      {
+        field: 'userId',
+        message: 'Invalid userId',
+      },
+    ] as ValueObjectExceptionType[];
+    const expectedDetails = [
+      {
+        field: 'userId',
+        message: 'Invalid userId',
+      },
+    ] as ValueObjectExceptionType[];
+    const exception = new DomainException(messageForException, details);
+
+    // Act
+    const detailsFromException = exception.details;
+
+    // Assert
+    expect(detailsFromException).toEqual(expectedDetails);
   });
 });
