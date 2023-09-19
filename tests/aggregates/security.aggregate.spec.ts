c1932c5bc0707b3e6a5363efec5cad258aa646d3
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-18 19:04:56 -0500

    test: :white_check_mark: se agregan pruebas unitarias para los agregados

--- tests/aggregates/security.aggregate.spec.ts
+++ tests/aggregates/security.aggregate.spec.ts
@@ -1,5 +1,41 @@
+import {
+  SecurityAggregate,
+  User,
+  UserEmailValueObject,
+  UserNameValueObject,
+  UserPasswordValueObject,
+} from '../../src';
+
+jest.mock('../../src/entities/user.entity.ts');
+
 describe('SecurityAggregate', () => {
-  it('should to be valid', () => {
-    expect(true).toBe(true);
+  describe('createUser', () => {
+    let name: UserNameValueObject;
+    let email: UserEmailValueObject;
+    let password: UserPasswordValueObject;
+    let securityAggregate: SecurityAggregate;
+
+    beforeAll(() => {
+      name = new UserNameValueObject('John Doe');
+      email = new UserEmailValueObject('jhon.doe@domain.co');
+      password = new UserPasswordValueObject('Password123!');
+      securityAggregate = new SecurityAggregate();
+    });
+
+    it('should create a new user', () => {
+      // Arrange
+      const data = { name, email, password };
+      const expectedData = { name, email, password };
+      jest.spyOn(User.prototype, 'create').mockImplementationOnce(() => {
+        return new User({});
+      });
+
+      // Act
+      securityAggregate.createUser(data);
+
+      // Assert
+      expect(User).toHaveBeenCalledWith(expectedData);
+      expect(User.prototype.create).toHaveBeenCalled();
+    });
   });
 });
--- tests/aggregates/to-do.aggregate.spec.ts
+++ tests/aggregates/to-do.aggregate.spec.ts
@@ -1,5 +1,98 @@
+import {
+  ToDo,
+  ToDoAggregate,
+  ToDoDescriptionValueObject,
+  ToDoIdValueObject,
+  ToDoTitleValueObject,
+  User,
+} from '../../src';
+
+jest.mock('../../src/entities/to-do.entity.ts');
+
 describe('ToDoAggregate', () => {
-  it('should to be valid', () => {
-    expect(true).toBe(true);
+  let toDoId: ToDoIdValueObject;
+  let title: ToDoTitleValueObject;
+  let description: ToDoDescriptionValueObject;
+  let user: User;
+  let toDoAggregate: ToDoAggregate;
+
+  beforeAll(() => {
+    toDoId = new ToDoIdValueObject('550e8400-e29b-41d4-a716-887744331234');
+    title = new ToDoTitleValueObject('title');
+    description = new ToDoDescriptionValueObject('description');
+    user = new User({});
+  });
+
+  describe('createToDo', () => {
+    it('should create a ToDo with all props', () => {
+      // Arrange
+      const data = {
+        toDoId,
+        title,
+        description,
+        user,
+      };
+      const expectedData = {
+        toDoId,
+        title,
+        description,
+      };
+      jest.spyOn(ToDo.prototype, 'create').mockImplementationOnce(() => {
+        return new ToDo({});
+      });
+      toDoAggregate = new ToDoAggregate();
+
+      // Act
+      toDoAggregate.createToDo(data);
+
+      // Assert
+      expect(ToDo).toHaveBeenCalledWith(expectedData);
+      expect(ToDo.prototype.create).toHaveBeenCalledWith(user);
+    });
+
+    it('should create a ToDo with minimal props', () => {
+      // Arrange
+      const data = {
+        title,
+        user,
+      };
+      const expectedData = {
+        title,
+      };
+      jest.spyOn(ToDo.prototype, 'create').mockImplementationOnce(() => {
+        return new ToDo({});
+      });
+      toDoAggregate = new ToDoAggregate();
+
+      // Act
+      toDoAggregate.createToDo(data);
+
+      // Assert
+      expect(ToDo).toHaveBeenCalledWith(expectedData);
+      expect(ToDo.prototype.create).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('completeToDo', () => {
+    it('should complete a ToDo', () => {
+      // Arrange
+      const data = {
+        toDoId,
+      };
+      const expectedData = {
+        toDoId,
+      };
+      jest.spyOn(ToDo.prototype, 'complete').mockImplementationOnce(() => {
+        return new ToDo({});
+      });
+      toDoAggregate = new ToDoAggregate();
+
+      // Act
+      toDoAggregate.completeToDo(data);
+
+      // Assert
+      expect(ToDo).toHaveBeenCalledWith(expectedData);
+      expect(ToDo.prototype.complete).toHaveBeenCalled();
+    });
   });
 });
